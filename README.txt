fda07d7aa2eb49bcc02e80625e283f222ff34410
Cloud-RF <farrant.alex@gmail.com>
2014-06-25 21:44:21 +0100

    Improved example
    Updated example with 10 2.4GHz sites
    Removed requests dependency!
    Added mesh demo

--- README.txt
+++ README.txt
@@ -1,15 +1,20 @@
 CloudRF Batchjob
 
 This Python script automates the generation of radio propagation calculations using the CloudRF API.
-To use it you will need a CloudRF account (free!), Python, the Python 'requests' library and Google earth.
+To use it you will need a CloudRF account, Python and Google earth.
+Your CloudRF API credentials can be obtained by emailing support@cloudrf.com
 
 Concept:
-The script reads in data from the CSV file, then performs a HTTP request to cloudrf.com for each row.
-The result is a link to a KMZ file which the script downloads and launches in Google earth.
+The script reads in data from the CSV file, then performs a HTTP POST request to https://web.cloudrf.com for each row. The result is a link to a KMZ file which the script downloads. It can also optionally automatically open the 
+KMZ in Google earth as it goes by setting a 'download' flag at the top of the script.
+
+Upon completion of each row, the script requests a mesh be created which stitches all the calculations on the
+server into a super layer which is also downloaded.
+
 
 API reference:
-The entire API is available at http://cloudrf.com/api
+The entire API is available at http://cloudrf.com/pages/api
 
 Support:
-Get help at cloudrf.com or email support@cloudrf.com
+Email support@cloudrf.com
  
--- calculate.py
+++ calculate.py
@@ -1,30 +1,50 @@
 #!/usr/bin/python
-import requests, csv, subprocess, os, urllib2, time, sys
+import csv, subprocess, os, urllib, urllib2, time, sys
 # CloudRF API client script Copyright 2014 Farrant Consulting Ltd
 #
-# Reads in radio transmitter data from data.csv and creates a propagation KMZ for each row
-# Once complete, it will download the KMZ and launch it with Google earth
+# Reads in radio transmitter data from a CSV files and creates a propagation KMZ for each row
+# Once complete, it will download the KMZ files
 # The CSV file MUST be formatted according to the example!
-# Before using you must create a CloudRF account and enter your UID and SHA1 password hash in the data fields 'uid' and 'key'
+# Before using you must create a CloudRF account and enter your API credentials in the data fields 'uid' and 'key'
 # For help email: support@cloudrf.com
 
-apiurl="http://10.0.0.6/API/api.php" # Public server 
-delay = 1 # Set to >8 for the public server or 0 if you own your own
+# CHANGE THESE SETTINGS
+server="https://web.cloudrf.com" # Public server 
+delay = 4 # Set to >8 for the public server or 0 if you own your own
 googleearth=0 # Set to 0 to NOT open google earth after a successful calc
-# DO NOT EDIT BELOW HERE
+debug=0 # Set to 1 to see RAW parameters pre-send
+domesh=1 # Set to 0 to only process sites. 1 to stitch together into super layer
+# DO NOT EDIT BELOW HERE!
+
+calcs = []
+uid = ""
 
 o = urllib2.build_opener( urllib2.HTTPCookieProcessor()) 
 
-# Send job to server. Refer to cloudrf.com/docs/api for API parameters.
+# Send job to server. Refer to cloudrf.com/pages/api for API parameters.
 def calculate(args):
+	global uid
 	nam = args.get('nam')
-	print "Calculating %s for %skm at %s pixels/degree..." % (nam,args.get('rad'),args.get('res'))
-	r = requests.post(apiurl, data=args)
-	if "http" in r.text:
-		print r.text+"\n"
-		#download(r.text,nam)
+	if args.get('uid') > 0:
+		uid = args.get('uid')
+	
+	print "\nCalculating %s for %skm at %s pixels/degree..." % (nam,args.get('rad'),args.get('res'))
+	
+	# Build POST request
+	data = urllib.urlencode(args)
+	req = urllib2.Request(server+"/API/api.php", data)
+	r = urllib2.urlopen(req)
+	
+	# Read in response. Hopefully a file http://...
+	result = r.read()
+	if "http" in result:
+		print result
+		calcs.append(result.replace(server+"/users/"+uid+"/",""))
+		
+		# Success! Download KMZ
+		download(result,nam)
 	else:
-		print r.text
+		print result
 
 # Download KMZ and launch in Google earth
 def download(file,nam):
@@ -41,7 +61,27 @@ def download(file,nam):
 		local_file.write(response)
 	print "Downloaded as "+localFile
 	if googleearth:
-		os.startfile(localFile) # Launch Google earth (if installed)
+		# Launch Google earth (if installed and associated with .kmz)
+		os.startfile(localFile) 
+	
+#Stitch calcs into super layer (mesh)	
+def mesh(calcs):
+	global uid
+	# Omit '&kmz=1' to receive a EPSG 3857 PNG and lat/lon bounds
+	meshurl=server+"/API/mesh/mesh.php?uid="+str(uid)+"&name=mesh&kmz=1&calcs="
+	print "\nStitching mesh..."
+	for layer in calcs:
+		meshurl=meshurl+layer.replace(".kmz\n","")+","
+	
+	# Fetch URL with http GET
+	req = urllib2.Request(meshurl)
+	r = urllib2.urlopen(req)
+	result = r.read()
+	if "http" in result:
+		print result
+		download(result,"mesh")
+	else:
+		print result
 	
 	
 if len(sys.argv) == 1:
@@ -54,9 +94,15 @@ if not os.path.exists("kmz"):
 # Open CSV file
 csvfile = csv.DictReader(open(sys.argv[1]))
 for row in csvfile:
+	# Pause script. Important otherwise server will refuse repeat requests
+	time.sleep(delay)
 	start_time = time.time() # Stopwatch start
-	print row
+	if debug:
+		print row
 	calculate(row)
 	elapsed = round(time.time() - start_time,1) # Stopwatch stop
-	#print "Elapsed time: "+str(elapsed)+"s"
-	time.sleep(delay)
+	print "Elapsed: "+str(elapsed)+"s"
+	
+
+# Mesh now
+mesh(calcs)
